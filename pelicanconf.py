2e38203a0f304e25b1d084c0e95ae13c74e27020
Nick Jones <nrjones8@gmail.com>
2019-05-19 19:37:49 -0400

    conf tweaks for new theme

--- pelicanconf.py
+++ pelicanconf.py
@@ -5,8 +5,12 @@ from __future__ import unicode_literals
 AUTHOR = u'Nick Jones'
 SITENAME = u'Nick Jones'
 SITEURL = ''
-
-TIMEZONE = ''
+# i am not creative
+SITETITLE = 'Nick Jones'
+# really, really not creative
+SITESUBTITLE = ''
+SITEDESCRIPTION = "Nick Jones's personal website and blog"
+#FAVICON = SITEURL + '/images/favicon.ico'
 
 DEFAULT_LANG = u'en'
 
@@ -15,8 +19,11 @@ FEED_ALL_ATOM = None
 CATEGORY_FEED_ATOM = None
 TRANSLATION_FEED_ATOM = None
 
-# The social sidebars are kinda noisy, just remove them
-SOCIAL = None
+
+SOCIAL = (
+    ('github', 'https://github.com/nrjones8'),
+    ('twitter', 'https://twitter.com/nrjones8')
+)
 
 DEFAULT_PAGINATION = False
 DISPLAY_PAGES_ON_MENU = False
@@ -28,6 +35,10 @@ EXTRA_PATH_METADATA = {
     'static/google71a1aae577e78d5a.html': {'path': 'google71a1aae577e78d5a.html'}
 }
 
+JINJA_ENVIRONMENT = {}
+THEME_STATIC_DIR = 'static'
+DISABLE_URL_HASH = True
+
 # http://docs.getpelican.com/en/stable/settings.html#url-settings
 PAGE_URL = '{slug}.html'
 PAGE_SAVE_AS = '{slug}.html'
@@ -35,6 +46,6 @@ PAGE_SAVE_AS = '{slug}.html'
 DEFAULT_DATE_FORMAT = '%B %d, %Y'
 
 
-THEME = 'themes/tuxlite_tbs'
+THEME = 'themes/Flex'
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
